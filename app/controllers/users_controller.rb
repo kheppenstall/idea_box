c5c1d0900fabc12f37ac6d69fdbe26ae6dbc9a2a
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-20 14:47:43 -0700

    Visitor can create an account.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Account successfully created!"
+      redirect_to user_path(@user)
+    else
+      flash.now[:danger] = @user.errors.full_messages.first
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
+  end
+end
--- spec/features/users/visitor_can_create_user_spec.rb
+++ spec/features/users/visitor_can_create_user_spec.rb
@@ -2,22 +2,42 @@ require 'rails_helper'
 
 RSpec.describe "User visits login page" do
   scenario "enters valid data to create an account" do
-
     visit new_user_path
 
     fill_in "user[name]", with: "Kyle"
     fill_in "user[email]", with: "kyle@example.com"
     fill_in "user[password]", with: "password"
-    fill_in "user[password_confirmation]", with: "password"
+
     click_on "Create Account"
 
-    expect(current_path).to eq user_path(User.first)
+    user = User.first
+
+    expect(current_path).to eq user_path(user)
     expect(page).to have_content "Account successfully created!"
     expect(page).to have_content "Logged in as Kyle"
-    expect(page).to have_content "kyle@example.com"
+    expect(page).to have_link "Logged in as Kyle", edit_user_path(user)
   end
 
-  # scenario "enters invalid data to create an account" do
+  scenario "enters invalid data to create an account" do
+    visit new_user_path
+
+    click_on "Create Account"
+
+    expect(page).to have_content "Name can't be blank"
+  end
+
+  scenario "enters email address already in database" do
+    user = create(:user)
+
+    visit new_user_path
+
+    fill_in "user[name]", with: user.name
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: "password"
+
+    click_on "Create Account"
+
+    expect(page).to have_content "Email has already been taken"
+  end
   
-  # end
 end
