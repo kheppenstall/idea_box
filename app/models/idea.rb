9a787e4ed60cbd98d90d166df7d6618807df79ee
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-20 20:11:47 -0700

    Idea model tests.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,6 @@
 class Idea < ApplicationRecord
+  validates :content, presence: true
+
   belongs_to :category
   belongs_to :user
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  
+  context "validations" do
+    it { is_expected.to validate_presence_of(:content) }
+  end
+
+  context "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:category) }
+    # it { should have_many(:image_ideas) }
+    # it { should have_many(:images, through: :image_ideas) }
+  end
 end
