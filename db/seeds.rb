61022ee7c77b4234e054a2ba37fe792ba753b614
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-20 20:20:46 -0700

    Include ideas in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
+FactoryGirl.create_list(:category, 10)
 
 5.times do |n|
-  FactoryGirl.create(:category)
+  user = FactoryGirl.create(:user)
+  5.times do |x|
+    offset = rand(Category.count)
+    idea = FactoryGirl.create(:idea, category: Category.offset(offset).first)
+    user.ideas << idea
+  end
 end
