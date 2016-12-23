221a9bc29995a8419cd5f49920a94c2a8c2fd298
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-12-22 17:20:29 -0700

    Add links to navigate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
 FactoryGirl.create_list(:category, 10)
 FactoryGirl.create_list(:image, 10)
 
+admin = User.create(name: "admin", password: "admin", email: "admin@admin.com", role: 1)
+
+5.times do |x|
+  offset = rand(Category.count)
+  idea = FactoryGirl.create(:idea, category: Category.offset(offset).first)
+  idea.images << Image.all.sample(2)
+  admin.ideas << idea
+end
+
 5.times do |n|
   user = FactoryGirl.create(:user)
   5.times do |x|
